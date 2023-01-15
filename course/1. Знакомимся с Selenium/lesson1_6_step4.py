c2a19c012c2f9ad0b136968bcd7ed467e8384888
Vyacheslav Ershov <v.ershov@ispsystem.com>
2023-01-15 10:09:42 +0800

    change indents in all lesson change locator for 'input1' in lesson1_6_step5.py

--- course/1. Знакомимся с Selenium/lesson1_6_step4.py
+++ course/1. Знакомимся с Selenium/lesson1_6_step4.py
@@ -9,12 +9,16 @@ with webdriver.Chrome() as driver_chrome:
 
     input1 = driver_chrome.find_element(By.TAG_NAME, 'input')
     input1.send_keys('Vyacheslav')
+
     input2 = driver_chrome.find_element(By.NAME, 'last_name')
     input2.send_keys('Ershov')
+
     input3 = driver_chrome.find_element(By.CLASS_NAME, 'city')
     input3.send_keys('Irkutsk')
+
     input4 = driver_chrome.find_element(By.ID, 'country')
     input4.send_keys('Russia')
+
     button = driver_chrome.find_element(By.CLASS_NAME, 'btn')
     button.click()
 
--- course/1. Знакомимся с Selenium/lesson1_6_step5.py
+++ course/1. Знакомимся с Selenium/lesson1_6_step5.py
@@ -12,14 +12,18 @@ with webdriver.Chrome() as driver_chrome:
     link = driver_chrome.find_element(By.LINK_TEXT, link_locator)
     link.click()
 
-    input1 = driver_chrome.find_element(By.TAG_NAME, 'input')
+    input1 = driver_chrome.find_element(By.CSS_SELECTOR, 'form>div:nth-child(1)>input')
     input1.send_keys('Vyacheslav')
+
     input2 = driver_chrome.find_element(By.NAME, 'last_name')
     input2.send_keys('Ershov')
+
     input3 = driver_chrome.find_element(By.CLASS_NAME, 'city')
     input3.send_keys('Irkutsk')
+
     input4 = driver_chrome.find_element(By.ID, 'country')
     input4.send_keys('Russia')
+
     button = driver_chrome.find_element(By.CLASS_NAME, 'btn')
     button.click()
 
